735e51ebd7d53a0cbf1d4f894f2cf1ab2e1c5eaa
rafidalazad <141198586+rafidalazad@users.noreply.github.com>
2023-08-11 21:09:34 +1000

    wk2main.c

--- KIT205Tutorials/wk2main.c
+++ KIT205Tutorials/wk2main.c
@@ -1,1 +1,23 @@
-#pragma once
+#include <stdio.h>
+#include "list.h"
+
+int main() {
+    List my_list = new_list();
+
+    int input;
+    scanf("%d", &input);
+
+    while (input != 0) {
+        insert_at_front(&my_list, input);
+        scanf("%d", &input);
+    }
+
+    printf("List contents: ");
+    print_list(&my_list);
+
+    // Testing other functions here
+
+    destroy_list(&my_list);
+
+    return 0;
+}
